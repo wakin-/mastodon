c3bbdb906e628cb0c7bf229039c9bf045a4855dd
Genbu Hase <hasegenbu@gmail.com>
2018-04-08 16:56:25 +0900

    [New] Implement a feature of quote

--- app/javascript/mastodon/containers/status_container.js
+++ app/javascript/mastodon/containers/status_container.js
@@ -4,6 +4,7 @@ import Status from '../components/status';
 import { makeGetStatus } from '../selectors';
 import {
   replyCompose,
+  quoteCompose,
   mentionCompose,
   directCompose,
 } from '../actions/compose';
@@ -82,6 +83,10 @@ const mapDispatchToProps = (dispatch, { intl }) => ({
     } else {
       dispatch(openModal('BOOST', { status, onReblog: this.onModalReblog }));
     }
+  },
+
+  onQuote (status, router) {
+    dispatch(quoteCompose(status, router));
   },
 
   onFavourite (status) {
