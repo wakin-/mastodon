80590c748adb3e69c8071820622cdefdddf17946
wakin- <8th.mail@gmail.com>
2018-06-20 10:49:51 +0900

    remake quote feature

--- db/schema.rb
+++ db/schema.rb
@@ -516,6 +516,7 @@ ActiveRecord::Schema.define(version: 2018_10_26_034033) do
     t.bigint "account_id", null: false
     t.bigint "application_id"
     t.bigint "in_reply_to_account_id"
+    t.bigint "quote_id"
     t.index ["account_id", "id", "visibility", "updated_at"], name: "index_statuses_20180106", order: { id: :desc }
     t.index ["in_reply_to_account_id"], name: "index_statuses_on_in_reply_to_account_id"
     t.index ["in_reply_to_id"], name: "index_statuses_on_in_reply_to_id"
